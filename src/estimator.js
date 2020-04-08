2f64c8e2863e2f422a355e072f0659c02aabe477
FrankKamala <frankkamala@gmail.com>
2020-04-08 16:32:08 +0300

    tab after//

--- src/estimator.js
+++ src/estimator.js
@@ -1,19 +1,19 @@
 const covid19ImpactEstimator = (data) => {
-//initial To estimate the number of infected people 30 days from now, note that currentlyInfected doubles
-//every 3 days, so you'd have to multiply it by a factor of 2.
+//  initial To estimate the number of infected people 30 days from now, note that currentlyInfected doubles
+//  every 3 days, so you'd have to multiply it by a factor of 2.
 
 const input = data;
 let periodDays = 0;
 let estimateNumber = currentlyInfected*1024;
 impact.currentlyInfected = data.reported*10;
 severeImpact.currentlyInfected = data.reported*50;
-//week/ days/moonths
+//  week/ days/moonths
 
 impact.infectionsByRequestedTime = impact.currentlyInfected*estimateNumber;
 severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected*estimateNumber;
 
-//estimate ==currentlyInfected x (2 to the power of *factor*) where factor is 10 for a 30 day
-//duration (there are 10 sets of 3 days in a perioid of 30 days)
+//  estimate ==currentlyInfected x (2 to the power of *factor*) where factor is 10 for a 30 day
+//  duration (there are 10 sets of 3 days in a perioid of 30 days)
 
 return {
     data: input,
