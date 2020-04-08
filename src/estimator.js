a0de401a7a2ad74eb7cda1a21ede5fbe5c8f393b
FrankKamala <frankkamala@gmail.com>
2020-04-08 15:52:26 +0300

    calc impact and Severe impact

--- src/estimator.js
+++ src/estimator.js
@@ -2,6 +2,10 @@ const covid19ImpactEstimator = (data) => {
 //initial
 
 const input = data;
+let periodDays = 0;
+impact.currentlyInfected = data.reported*10;
+severeImpact.currentlyInfected = data.reported*50;
+
 return {
     data: input,
     impact: {},
