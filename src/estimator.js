676751a9bbd9e9bdf3f344183d96189bcb017ddf
FrankKamala <frankkamala@gmail.com>
2020-04-08 16:22:35 +0300

    add infection by time * estimate number

--- src/estimator.js
+++ src/estimator.js
@@ -8,8 +8,8 @@ let estimateNumber = currentlyInfected*1024;
 impact.currentlyInfected = data.reported*10;
 severeImpact.currentlyInfected = data.reported*50;
 
-impact.infectionsByRequestedTime = impact.currentlyInfected;
-severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected;
+impact.infectionsByRequestedTime = impact.currentlyInfected*estimateNumber;
+severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected*estimateNumber;
 
 //estimate ==currentlyInfected x (2 to the power of *factor*) where factor is 10 for a 30 day
 //duration (there are 10 sets of 3 days in a perioid of 30 days)
